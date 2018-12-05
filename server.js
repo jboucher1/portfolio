b24d62edacccd347be905ccd72f8e59f7d4566a5
Jess Boucher <boucherjess@gmail.com>
2018-12-05 16:45:03 +1300

    project name change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/jess-portfolio-v2'));
+app.use(express.static(__dirname + '/dist/portfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/jess-portfolio-v2/index.html'));
+res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
